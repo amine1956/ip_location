2f791b9f670c98f28f1aa5b0e3b3807a4d30d898
amine1956 <amineklm983@gmail.com>
2024-11-10 12:14:15 +0100

    add vue

--- ip.py
+++ ip.py
@@ -1,22 +1,20 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import requests
 import uvicorn
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")  # Directory for HTML templates
 
 def get_client_ip(request: Request) -> str:
-    # Attempt to retrieve the 'X-Envoy-External-Address' header
     client_ip = request.headers.get("X-Envoy-External-Address")
-
-    # Fallback to 'client.host' if the header is absent or None
     if client_ip is None:
         client_ip = request.client.host
-
     return client_ip
 
 def get_location(ip: str) -> dict:
     try:
-        # Use ip-api.com to get location data for the IP address
         response = requests.get(f"http://ip-api.com/json/{ip}")
         if response.status_code == 200:
             return response.json()
@@ -25,11 +23,12 @@ def get_location(ip: str) -> dict:
     except Exception as e:
         return {"error": str(e)}
 
-@app.get("/client-location")
+@app.get("/", response_class=HTMLResponse)
 async def client_location(request: Request):
     ip = get_client_ip(request)
     location_data = get_location(ip)
-    return {"client_ip": ip, "location": location_data}
+    print(location_data)  # Affiche les données pour débogage
+    return templates.TemplateResponse("map.html", {"request": request, "location": location_data})
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
